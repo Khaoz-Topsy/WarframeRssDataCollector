e47c1c1622b09eb60a0d5fe4c9efc5c743b810d4
kurt.lourens <kurt.lourens@entelect.co.za>
2017-04-28 19:19:55 +0200

    First commit

--- WarframeRssDataCollector.Data/Repositories/DatabaseRepository.cs
+++ WarframeRssDataCollector.Data/Repositories/DatabaseRepository.cs
@@ -19,17 +19,195 @@ namespace WarframeRssDataCollector.Data.Repositories
 
         public async void saveAlerts(List<WarframeAlertItem> Alerts)
         {
-			
+            foreach (WarframeAlertItem alert in Alerts)
+            {
+                int retries = 0;
+                bool passed = false;
+
+                do
+                {
+                    try
+                    {
+                        using (SqlConnection conn = new SqlConnection(connStr.getString()))
+                        {
+                            WarframeAlertInformation info = new WarframeAlertInformation(alert);
+
+                            if (alert.isValid() && info.isValid())
+                            {
+                                await conn.OpenAsync();
+                                string query = "INSERT INTO " + SecretData.TableAlertData +
+                                            "(" +
+                                                "GUID, Title, Author, Descrip, Money, " +
+                                                "Reward, RewardType, Location, Faction, PubDate, Expiry" +
+                                            ") " +
+                                        "VALUES " +
+                                            "(" +
+                                                "@guid, @title, @author, @desc, @money, " +
+                                                "@reward, @rewardtype, @location, @faction, @pubdate, @expiry" +
+                                            "); ";
+                                SqlCommand command = new SqlCommand(query, conn);
+                                command.Parameters.AddWithValue("@guid", alert.Guid);
+                                command.Parameters.AddWithValue("@title", alert.Title);
+                                command.Parameters.AddWithValue("@author", alert.Author);
+                                command.Parameters.AddWithValue("@desc", alert.Description);
+                                command.Parameters.AddWithValue("@money", info.Money);
+
+                                command.Parameters.AddWithValue("@reward", info.Reward);
+                                command.Parameters.AddWithValue("@rewardtype", info.RewardType);
+                                command.Parameters.AddWithValue("@location", info.Location);
+                                command.Parameters.AddWithValue("@faction", alert.Faction);
+                                command.Parameters.AddWithValue("@pubdate", alert.PubDate);
+                                command.Parameters.AddWithValue("@expiry", alert.Expiry);
+
+                                await command.ExecuteNonQueryAsync();
+
+                                passed = true;
+                                Console.WriteLine("DB Entry made: " + alert.Title);
+                            }
+                            else
+                            {
+                                Console.WriteLine(
+                                    "Alert is Valid: " + alert.isValid() +
+                                    "Info is Valid: " + info.isValid()
+                                    );
+                            }
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        retries++;
+                        if (retries == 1) { Console.WriteLine("DB Entry failed for: " + alert.Title); }
+                        else { Console.WriteLine("\t Attempt #" + retries); }
+                    }
+                }
+                while ((retries < 3) && (passed == false));
+            }
         }
 
         public async void saveInvasions(List<WarframeNonAlertItem> Invasions)
         {
-			
+            foreach (WarframeNonAlertItem invasion in Invasions)
+            {
+                int retries = 0;
+                bool passed = false;
+
+                do
+                {
+                    try
+                    {
+                        using (SqlConnection conn = new SqlConnection(connStr.getString()))
+                        {
+                            WarframeInvasionInformation info = new WarframeInvasionInformation(invasion);
+
+                            if (invasion.isValid() && info.isValid())
+                            {
+                                await conn.OpenAsync();
+                                string query = "INSERT INTO " + SecretData.TableInvasionData +
+                                            "(" +
+                                                "GUID, Title, Author, PubDate, Faction1, " +
+                                                "Faction1Reward, Faction2, Faction2Reward, Location" +
+                                            ") " +
+                                        "VALUES " +
+                                            "(" +
+                                                "@guid, @title, @author, @pubdate, @fac1, " +
+                                                "@fac1reward, @fac2, @fac2reward, @location" +
+                                            "); ";
+                                SqlCommand command = new SqlCommand(query, conn);
+                                command.Parameters.AddWithValue("@guid", invasion.Guid);
+                                command.Parameters.AddWithValue("@title", invasion.Title);
+                                command.Parameters.AddWithValue("@author", invasion.Author);
+                                command.Parameters.AddWithValue("@pubdate", invasion.PubDate);
+                                command.Parameters.AddWithValue("@fac1", info.faction1);
+                                command.Parameters.AddWithValue("@fac2", info.faction2);
+                                command.Parameters.AddWithValue("@fac1reward", info.faction1Reward);
+                                command.Parameters.AddWithValue("@fac2reward", info.faction2Reward);
+                                command.Parameters.AddWithValue("@location", info.location);
+
+                                await command.ExecuteNonQueryAsync();
+
+                                passed = true;
+                                Console.WriteLine("DB Entry made: " + invasion.Title);
+                            }
+                            else
+                            {
+                                Console.WriteLine(
+                                    "Invasion is Valid: " + invasion.isValid() +
+                                    "Info is Valid: " + info.isValid()
+                                    );
+                            }
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        retries++;
+                        if (retries == 1) { Console.WriteLine("DB Entry failed for: " + invasion.Title); }
+                        else { Console.WriteLine("\t Attempt #" + retries); }
+                    }
+                }
+                while ((retries < 3) && (passed == false));
+            }
         }
 
         public async void saveOutbreaks(List<WarframeNonAlertItem> Outbreaks)
         {
-			
+            foreach (WarframeNonAlertItem outbreak in Outbreaks)
+            {
+                int retries = 0;
+                bool passed = false;
+
+                do
+                {
+                    try
+                    {
+                        using (SqlConnection conn = new SqlConnection(connStr.getString()))
+                        {
+                            WarframeOutbreakInformation info = new WarframeOutbreakInformation(outbreak);
+
+                            if (outbreak.isValid() && info.isValid())
+                            {
+                                await conn.OpenAsync();
+                                string query = "INSERT INTO " + SecretData.TableOutbreakData +
+                                            "(" +
+                                                "GUID, Title, Author, PubDate, " +
+                                                "Reward, Location, isPhorid" +
+                                            ") " +
+                                        "VALUES " +
+                                            "(" +
+                                                "@guid, @title, @author, @pubdate, " +
+                                                "@reward, @location, @isphorid" +
+                                            "); ";
+                                SqlCommand command = new SqlCommand(query, conn);
+                                command.Parameters.AddWithValue("@guid", outbreak.Guid);
+                                command.Parameters.AddWithValue("@title", outbreak.Title);
+                                command.Parameters.AddWithValue("@author", outbreak.Author);
+                                command.Parameters.AddWithValue("@pubdate", outbreak.PubDate);
+                                command.Parameters.AddWithValue("@reward", info.Reward);
+                                command.Parameters.AddWithValue("@location", info.Location);
+                                command.Parameters.AddWithValue("@isphorid", info.isPhorid);
+
+                                await command.ExecuteNonQueryAsync();
+
+                                passed = true;
+                                Console.WriteLine("DB Entry made: " + outbreak.Title);
+                            }
+                            else
+                            {
+                                Console.WriteLine(
+                                    "Outbreak is Valid: " + outbreak.isValid() +
+                                    "Info is Valid: " + info.isValid()
+                                    );
+                            }
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        retries++;
+                        if (retries == 1) { Console.WriteLine("DB Entry failed for: " + outbreak.Title); }
+                        else { Console.WriteLine("\t Attempt #" + retries); }
+                    }
+                }
+                while ((retries < 3) && (passed == false));
+            }
         }
 
         public void deleteFiles()
--- WarframeRssDataCollector.Data/Repositories/PushJetRepository.cs
+++ WarframeRssDataCollector.Data/Repositories/PushJetRepository.cs
@@ -13,7 +13,31 @@ namespace WarframeRssDataCollector.Data.Repositories
     {
         public async void SendMessage(PushJetMessageContent messageData)
         {
-                       
+            await Task.Run(() =>
+            {
+                try
+                {
+                    var httpWebRequest = (HttpWebRequest)WebRequest.Create("https://api.pushjet.io/message");
+
+                    //place body here
+                    string stringData =
+                        "secret=" + messageData.secret + "&" +
+                        "message=" + messageData.message + "&" +
+                        "title=" + messageData.title + "&" +
+                        "level=" + messageData.level + "&" +
+                        "link=" + messageData.link;
+                    var data = Encoding.ASCII.GetBytes(stringData);
+
+                    httpWebRequest.Method = "POST";
+                    httpWebRequest.ContentType = "application/x-www-form-urlencoded";
+                    httpWebRequest.ContentLength = data.Length;
+
+                    Stream newStream = httpWebRequest.GetRequestStream();
+                    newStream.Write(data, 0, data.Length);
+                    newStream.Close();
+                }
+                catch (Exception) { }
+            });            
         }
     }
 }
